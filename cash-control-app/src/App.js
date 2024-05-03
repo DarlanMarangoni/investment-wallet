9d2d12011f26439d02acd5c762d5144a48ef64b0
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-05-03 14:42:52 -0300

    Criando navegacao com react router dom

--- cash-control-app/src/App.js
+++ cash-control-app/src/App.js
@@ -1,9 +1,23 @@
 import NavBar from './components/NavBar';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ExpensePage from "./pages/ExpensePage";
+import IncomePage from "./pages/IncomePage";
+import Container from "@mui/material/Container";
 
 function App() {
-    return (
-        <NavBar />
-    );
+    return (<>
+            <BrowserRouter>
+                <NavBar/>
+                <Container>
+                    <Routes>
+                        <Route path="/home" element={<HomePage/>}/>
+                        <Route path="/income" element={<IncomePage/>}/>
+                        <Route path="/expense" element={<ExpensePage/>}/>
+                    </Routes>
+                </Container>
+            </BrowserRouter>
+        </>);
 }
 
 export default App;
--- cash-control-app/src/components/NavBar/index.tsx
+++ cash-control-app/src/components/NavBar/index.tsx
@@ -1,23 +1,120 @@
-import { AppBar, Container, IconButton, Toolbar, Typography } from '@mui/material';
-import AdbIcon from '@mui/icons-material/Adb'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { useNavigate } from 'react-router-dom';
+
+const pages = ['Home', 'Expense', 'Income'];
 
 export default function NavBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+    let navigate = useNavigate();
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = (path: string) => {
+        navigate(path.toLowerCase())
+    };
+
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
-                <Toolbar>
-                    <AdbIcon sx={{ display: {xs: 'none', md: 'flex'}, mr: 1}} />
-                    <IconButton
-                        size="large"
-                        edge="end"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{mr: 2}}
+                <Toolbar disableGutters>
+                    <AdbIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 1}}/>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: {xs: 'none', md: 'flex'},
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
                     >
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        Controle financeiro
                     </Typography>
+
+                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: {xs: 'block', md: 'none'},
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={() => handleCloseNavMenu(`/${page}`)}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AdbIcon sx={{display: {xs: 'flex', md: 'none'}, mr: 1}}/>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: {xs: 'flex', md: 'none'},
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={() => handleCloseNavMenu(`/${page}`)}
+                                sx={{my: 2, color: 'white', display: 'block'}}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
