f959a5fc0a964c1b8f20abf01abb05f99ee95eed
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-05-05 20:58:36 -0300

    income form

--- cash-control-app/src/pages/Income/index.tsx
+++ cash-control-app/src/pages/Income/index.tsx
@@ -1,5 +1,36 @@
+import './style.css';
+
 const Income = () => {
-  return (<h1>Income</h1>);
-}
+  return (
+    <div className="container base-card pb-2 pt-2 main-income">
+      <h1>Cadastro de Receitas</h1>
+      <form>
+        <div className="row">
+          <div className="col-3">
+          <label htmlFor="select" className="form-label">Selecione a receita</label>
+            <select className="form-select" aria-label="Default select example">
+              <option selected>Selecione...</option>
+              <option value="salario">Salário</option>
+              <option value="dividendo">Dividendo</option>
+            </select>
+          </div>
+          <div className="mb-2 col-3">
+            <label htmlFor="inputProjectedValue" className="form-label">Valor previsto</label>
+            <input type="number" className="form-control" id="inputProjectedValue" aria-describedby="valorprevisto" />
+          </div>
+          <div className="mb-2 col-3">
+            <label htmlFor="inputActualValue" className="form-label">Valor realizado</label>
+            <input type="number" className="form-control" id="inputActualValue" aria-describedby="valorrealizado" />
+          </div>
+          <div className="mb-2 col-3">
+            <label htmlFor="inputDate" className="form-label">Data</label>
+            <input type="date" className="form-control" id="inputDate" aria-describedby="data" />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Salvar</button>
+      </form>
+    </div>
+  );
+};
 
 export default Income;
