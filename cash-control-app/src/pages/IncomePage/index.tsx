47c32e65198d2465ebc7d0a368f538f5940416f7
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-05-06 16:21:31 -0300

    form with react hook form

--- cash-control-app/src/pages/IncomePage/index.tsx
+++ cash-control-app/src/pages/IncomePage/index.tsx
@@ -3,8 +3,17 @@ import { useEffect, useState } from 'react';
 import { Income } from '../../Types/Income';
 import axios from 'axios';
 import { CASH_CONTROL_API_URL } from '../../services/incomeService';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Receive } from '../../Types/Receive';
 
 const IncomePage = () => {
+
+  const {
+    register,
+    handleSubmit
+  } = useForm<Receive>()
+  const onSubmit: SubmitHandler<Receive> = (data) => console.log(data)
+
 
   const [incomes, setIncomes] = useState<Income[]>();
 
@@ -19,7 +28,7 @@ const IncomePage = () => {
   return (
     <div className="container base-card pb-2 pt-2 main-income">
       <h1>Cadastro de Receitas</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="row">
           <div className="col-3">
             <label htmlFor="select" className="form-label">Selecione a receita</label>
@@ -30,20 +39,20 @@ const IncomePage = () => {
           </div>
           <div className="mb-2 col-3">
             <label htmlFor="inputProjectedValue" className="form-label">Valor previsto</label>
-            <input type="number" className="form-control" id="inputProjectedValue" aria-describedby="valorprevisto" />
+            <input type="number" className="form-control" id="inputProjectedValue" aria-describedby="valorprevisto" {...register("projected")}/>
           </div>
           <div className="mb-2 col-3">
             <label htmlFor="inputActualValue" className="form-label">Valor realizado</label>
-            <input type="number" className="form-control" id="inputActualValue" aria-describedby="valorrealizado" />
+            <input type="number" className="form-control" id="inputActualValue" aria-describedby="valorrealizado" {...register("actual")}/>
           </div>
           <div className="mb-2 col-3">
             <label htmlFor="inputDate" className="form-label">Data</label>
-            <input type="date" className="form-control" id="inputDate" aria-describedby="data" />
+            <input type="date" className="form-control" id="inputDate" aria-describedby="data" {...register("date")}/>
           </div>
         </div>
         <div className="mb-2">
           <label htmlFor="textAreaDescription" className="form-label">Descrição</label>
-          <textarea className="form-control" aria-label="With textarea"></textarea>
+          <textarea className="form-control" aria-label="With textarea" {...register("description")}></textarea>
         </div>
         <button type="submit" className="btn btn-primary mb-3">Salvar</button>
       </form>
