76f8e40f70a1457a022d1853a895ef211abd96b3
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-07-14 12:16:47 -0300

    removendo function

--- investment-history/src/main/java/com/wallet/investmenthistory/StockRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wallet.investmenthistory;
-
-import com.wallet.investmenthistory.domain.Stock;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class StockRepository implements PanacheRepository<Stock> {}
--- investment-history/src/main/java/com/wallet/investmenthistory/service/CustomCSVReader.java
+++
@@ -1,9 +0,0 @@
-package com.wallet.investmenthistory.service;
-
-import com.opencsv.exceptions.CsvValidationException;
-
-import java.io.IOException;
-
-public interface CustomCSVReader<T> {
-    T toList(String csv) throws CsvValidationException, IOException;
-}
