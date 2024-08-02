cb8ab651eb02ac9f91ecdb09c716f4e8cac9c5c3
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-08-02 15:23:20 -0300

    add prime react

--- investment-app/src/App.tsx
+++ investment-app/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
 import './App.css';
+import 'primereact/resources/themes/lara-dark-blue/theme.css';
+
+import { Button } from 'primereact/button';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
+        <Button label='Click me'/>
     </div>
   );
 }
