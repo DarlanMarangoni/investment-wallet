68381c256b6f484538efd376030cfa3c1c983fcd
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-08-04 20:23:32 -0300

    create menu

--- investment-app/src/App.tsx
+++ investment-app/src/App.tsx
@@ -1,15 +1,19 @@
 import './App.css';
 import 'primereact/resources/themes/lara-dark-blue/theme.css';
-
-import { Button } from 'primereact/button';
+import 'primeicons/primeicons.css';
+import { Menubar } from 'primereact/menubar';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-        <Button label='Click me'/>
-    </div>
-  );
+    const items = [
+        {label: 'Home', icon: 'pi pi-fw pi-home'},
+        {label: 'Ações', icon: 'pi pi-fw pi-dollar'},
+        {label: 'Fundos imobiliários', icon: 'pi pi-fw pi-building'}
+    ]
+    return (
+        <div className="App">
+            <Menubar model={items}/>
+        </div>
+    );
 }
 
 export default App;
