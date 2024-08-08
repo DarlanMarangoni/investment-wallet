f5adf80f718f2edafcf3f3e90fb1e981af67b88c
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-08-08 12:04:46 -0300

    add navigate

--- investment-app/src/App.tsx
+++ investment-app/src/App.tsx
@@ -3,25 +3,21 @@ import 'primereact/resources/themes/lara-dark-blue/theme.css';
 import 'primeicons/primeicons.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import { Menubar } from 'primereact/menubar';
 import Stock from './pages/Stock';
 import RealStateFund from './pages/RealStateFund';
+import { Menu } from './components/Menu';
+
 
 function App() {
-    const items = [
-        {label: 'Home', icon: 'pi pi-fw pi-home'},
-        {label: 'Ações', icon: 'pi pi-fw pi-dollar'},
-        {label: 'Fundos imobiliários', icon: 'pi pi-fw pi-building'}
-    ]
     return (
-            <BrowserRouter>
-                <Menubar model={ items }/>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/Stock" element={<Stock />} />
-                    <Route path="/RealStateFund" element={<RealStateFund />} />
-                </Routes>
-            </BrowserRouter>
+        <BrowserRouter>
+            <Menu/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/Stock" element={<Stock/>}/>
+                <Route path="/RealStateFund" element={<RealStateFund/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
