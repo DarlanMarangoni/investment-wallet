d629bac0b4dd365625dc754fe5fc39ad356d6479
Darlan Marangoni <marangoni.darlan1@gmail.com>
2025-08-26 21:39:49 -0300

    feat: add total

--- investment-api/src/main/java/com/darlanmarangoni/investmentapi/investment/resources/InvestmentResource.java
+++ investment-api/src/main/java/com/darlanmarangoni/investmentapi/investment/resources/InvestmentResource.java
@@ -1,14 +1,18 @@
 package com.darlanmarangoni.investmentapi.investment.resources;
 
+import com.darlanmarangoni.investmentapi.enums.InvestmentType;
 import com.darlanmarangoni.investmentapi.investment.domain.Investment;
 import com.darlanmarangoni.investmentapi.investment.dto.InvestmentDto;
 import com.darlanmarangoni.investmentapi.investment.repositories.InvestmentRepository;
+import com.darlanmarangoni.investmentapi.repositories.RealStateFundRepository;
+import com.darlanmarangoni.investmentapi.repositories.StockRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,9 +22,15 @@ public class InvestmentResource {
 
     public static final UUID USER_ID = UUID.fromString("a233a502-5a2e-4232-bd02-e1969942b58f");
     private final InvestmentRepository investmentRepository;
+    private final StockRepository stockRepository;
+    private final RealStateFundRepository realStateFundRepository;
 
-    public InvestmentResource(InvestmentRepository investmentRepository) {
+    public InvestmentResource(InvestmentRepository investmentRepository,
+                              StockRepository stockRepository,
+                              RealStateFundRepository realStateFundRepository) {
         this.investmentRepository = investmentRepository;
+        this.stockRepository = stockRepository;
+        this.realStateFundRepository = realStateFundRepository;
     }
 
     @GetMapping
@@ -34,6 +44,16 @@ public class InvestmentResource {
         investments.forEach(investment -> investment.setActive(false));
         investmentRepository.saveAll(investments);
         var entity = Investment.from(dto);
+        if (entity.getInvestmentType().equals(InvestmentType.STOCK)) {
+            stockRepository.findFirstByTickerOrderByDatCreationDesc(dto.ticker()).ifPresent(stock -> {
+                entity.setTotal(stock.getPreco().multiply(BigDecimal.valueOf(entity.getAmount())));
+            });
+        }
+        if (entity.getInvestmentType().equals(InvestmentType.FII)) {
+            realStateFundRepository.findFirstByTickerOrderByDatCreationDesc(dto.ticker()).ifPresent(fii -> {
+                entity.setTotal(fii.getPreco().multiply(BigDecimal.valueOf(entity.getAmount())));
+            });
+        }
         entity.setUserId(USER_ID);
         return investmentRepository.save(entity);
     }
